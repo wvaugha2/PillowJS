dd8e6198a0a8635cb18eb90ad32431f8c5b1147d
Ty Vaughan <tvaughan94@gmail.com>
2020-08-26 01:02:52 -0400

    Pushing initial version of PillowJS to repo

--- README.md
+++ README.md
@@ -1,2 +1,119 @@
 # PillowJS
-REST Easy
+
+## REST Easy with PillowJS!
+Our goal is to give you the tools to easily perform REST calls the way you want, using a library that won't be deprecated anytime soon! With the pillow-rest-js package, you'll be able to make calls using simplified functions for each REST method, or build requests step by step using our step-wise methods.
+
+## Incredibly easy to get started
+Making a request with PillowJS is as simple as it is shown below
+```javascript
+// Import the package
+const pillow = require('pillow-rest-js');
+
+// Create and perform the request
+const request = pillow.getRequest();
+const response = await request.get('http://destination.com').call()
+```
+
+## Different Ways to Make a Request
+With PillowJS, we wanted to offer you the ability to build out the request by chaining functions in addition to provided standardized functions to perform GET, POST, PUT, PATCH, and DELETE calls.
+
+### Step-wise Request Building
+By using our step-wise request building, you can build the request object piece by piece, making it easy to visualize exactly what the request contains. Because you are building a request object, you can pass this object across functions, modify the request, and call it multiple times making it easy to store and reuse requests any way you like. 
+
+Below is an example showing all of the methods available to be used to modify your request:
+```javascript
+// Create the request and specify which HTTP method and URL the request will use
+const request = pillow.getRequest();
+request.get('http://endpointUrl')
+
+  // Perform Basic Auth or add a Bearer Token for authorization
+  .authByUser('username', 'password')
+  .authByToken('bearer token')
+
+  // Modify the headers and query parameters for the request
+  .options({ headers: {}, params: {} })
+  .setHeaders({})
+  .addHeader('header', 'value')
+  .setParams({})
+  .addParam('param', 'value')
+
+  // Set the payload for the request
+  .setBody(bodyData, '"content-type" header value')
+
+  // Set how long to wait until timing out the request (in milliseconds)
+  .setTimeout(12000)
+
+  // If running in a browser, set the request to store cookies received in the response
+  .withCredentials()
+
+  // Instead of receiving a promise with the response data, set a callback to run
+  .callback((error, response, body) => {})
+  .onSuccess((response, body) => {})
+  .onError((error) => {})
+
+  // Call this method in order to kick off the request
+  .call();
+```
+
+Here's an example of making a request using the step-wise methods:
+```javascript
+const request = pillow.getRequest();
+const response = await request.post('http://posturl.com').setBody({color: 'red'}).call();
+```
+
+### Standardized Methods
+If you prefer to use a standardized way of performing your requests, we offer five methods, one for performing each of the five main Http methods.
+
+Below is an example of what arguments these functions accept:
+```javascript
+// GET/DELETE Request
+pillow.get/.delete(
+  url: string,
+  options: {headers?: {}, params?: {}},
+  auth?: { username?: string, password?: string } || { token?: string },
+  withCredentials?: boolean
+  callback?: (error, response, body) => {}
+)
+
+// POST/PUT/PATCH Request
+pillow.post/.put/.patch(
+  url: string,
+  body: any,
+  options: {headers?: {}, params?: {}},
+  auth?: { username?: string, password?: string } || { token?: string },
+  withCredentials?: boolean
+  callback?: (error, response, body) => {}
+)
+```
+
+Here's an example of making the same request as above using the standardized methods:
+```javascript
+const response = await pillow.post('http://posturl.com', {color: 'red'});
+```
+
+## Callbacks or Promises?
+You do you, big dog! With PillowJS, you can choose to use callbacks or promises. You can specify a general callback to handle both success and error responses, a separate callback for both success and error, or no callbacks to receive a Promise. See the examples below of how to make a REST call in all three ways:
+```javascript
+// Process the response from downstream using a single callback
+const request = pillow.getRequest();
+request.get('http://endpointUrl')
+  .callback((error, response, body) => {})
+  .call();
+
+// Process the responses from downstream using a success and error callback
+const request = pillow.getRequest();
+request.get('http://endpointUrl')
+  .onSuccess((response, body) => {})
+  .onError((error) => {})
+  .call();
+
+// Get the direct response from downstream as a JSON Promise containing the properties error, response, and body
+const request = pillow.getRequest();
+const result = await request.get('http://endpointUrl').call();
+```
+
+
+## Future Progress
+With PillowJS, our goal is to continue to expand the capabilities so that it can be the easiest go-to solution for making Http calls. This includes adding support for features such as streaming requests, piping requests together, supporting form data as a specific payload type that can be used, and more.
+
+If you'd like to help make PillowJS the best it can be, contact us through Github!
---
+++ index.js
@@ -0,0 +1,169 @@
+const PillowRequest = require('./lib/PillowRequest');
+
+/**
+ * @function getRequest
+ * @description This function returns a PillowRequest object, allowing you to build the REST request using the provided step-wise functions
+ * @param {boolean} [useHttps=false] - true if https should be used, false otherwise
+ * @returns {PillowRequest}
+ */
+module.exports.getRequest = (useHttps=false) => {
+  return new PillowRequest(useHttps);
+}
+
+/**
+ * @function get
+ * @description This function performs a GET request using the provided inputs
+ * @param {string} url - the full destination url for the request
+ * @param {{headers?: {[header: string]: string|string[]}, params?: {[param: string]: string|string[]}}} options - the headers and parameters to send with the request
+ * @param {{username?: string, password?: string, token?: string}} [auth] - the username and password or token value to use for authentication
+ * @param {boolean} [withCredentials] - if true and executing in a browser, stores the cookies sent in the request response; if false, does not
+ * @param {(error, response, body) => {}} [callback] - an anonymous function to use as a callback; if provided, executes the callback with the response information
+ * @returns {Promise<{error: Error, response: Object, body: Object}>} Returns a promise of the response information if no callback was provided
+ */
+module.exports.get = async (url, options, auth, withCredentials, callback) => {
+  // Create the request object and set the main request parameters
+  const request = new PillowRequest();
+  request.get(url).options(options).withCredentials(withCredentials);
+
+  // Add the provided type of auth
+  if (auth && auth.username && auth.password) {
+    request.authByUser(auth.username, auth.password);
+  } else if (auth && auth.token) {
+    request.authByToken(auth.token);
+  }
+
+  // Add the callback or return a promise
+  if (callback && typeof callback === 'function') {
+    request.callback(callback).call();
+    return;
+  } else {
+    return await request.call();
+  }
+}
+
+/**
+ * @function post
+ * @description This function performs a POST request using the provided inputs
+ * @param {string} url - the full destination url for the request
+ * @param {any} body - the payload to be sent in the request
+ * @param {{headers?: {[header: string]: string|string[]}, params?: {[param: string]: string|string[]}}} options - the headers and parameters to send with the request
+ * @param {{username?: string, password?: string, token?: string}} [auth] - the username and password or token value to use for authentication
+ * @param {boolean} [withCredentials] - if true and executing in a browser, stores the cookies sent in the request response; if false, does not
+ * @param {(error, response, body) => {}} [callback] - an anonymous function to use as a callback; if provided, executes the callback with the response information
+ * @returns {Promise<{error: Error, response: Object, body: Object}>} Returns a promise of the response information if no callback was provided
+ */
+module.exports.post = async (url, body, options, auth, withCredentials, callback) => {
+  // Create the request object and set the main request parameters
+  const request = new PillowRequest();
+  request.post(url).setBody(body).options(options).withCredentials(withCredentials);
+
+  // Add the provided type of auth
+  if (auth && auth.username && auth.password) {
+    request.authByUser(auth.username, auth.password);
+  } else if (auth && auth.token) {
+    request.authByToken(auth.token);
+  }
+
+  // Add the callback or return a promise
+  if (callback && typeof callback === 'function') {
+    request.callback(callback).call();
+    return;
+  } else {
+    return await request.call();
+  }
+}
+
+/**
+ * @function put
+ * @description This function performs a PUT request using the provided inputs
+ * @param {string} url - the full destination url for the request
+ * @param {any} body - the payload to be sent in the request
+ * @param {{headers?: {[header: string]: string|string[]}, params?: {[param: string]: string|string[]}}} options - the headers and parameters to send with the request
+ * @param {{username?: string, password?: string, token?: string}} [auth] - the username and password or token value to use for authentication
+ * @param {boolean} [withCredentials] - if true and executing in a browser, stores the cookies sent in the request response; if false, does not
+ * @param {(error, response, body) => {}} [callback] - an anonymous function to use as a callback; if provided, executes the callback with the response information
+ * @returns {Promise<{error: Error, response: Object, body: Object}>} Returns a promise of the response information if no callback was provided
+ */
+module.exports.put = async (url, body, options, auth, withCredentials, callback) => {
+  // Create the request object and set the main request parameters
+  const request = new PillowRequest();
+  request.put(url).setBody(body).options(options).withCredentials(withCredentials);
+
+  // Add the provided type of auth
+  if (auth && auth.username && auth.password) {
+    request.authByUser(auth.username, auth.password);
+  } else if (auth && auth.token) {
+    request.authByToken(auth.token);
+  }
+
+  // Add the callback or return a promise
+  if (callback && typeof callback === 'function') {
+    request.callback(callback).call();
+    return;
+  } else {
+    return await request.call();
+  }
+}
+
+/**
+ * @function patch
+ * @description This function performs a PATCH request using the provided inputs
+ * @param {string} url - the full destination url for the request
+ * @param {any} body - the payload to be sent in the request
+ * @param {{headers?: {[header: string]: string|string[]}, params?: {[param: string]: string|string[]}}} options - the headers and parameters to send with the request
+ * @param {{username?: string, password?: string, token?: string}} [auth] - the username and password or token value to use for authentication
+ * @param {boolean} [withCredentials] - if true and executing in a browser, stores the cookies sent in the request response; if false, does not
+ * @param {(error, response, body) => {}} [callback] - an anonymous function to use as a callback; if provided, executes the callback with the response information
+ * @returns {Promise<{error: Error, response: Object, body: Object}>} Returns a promise of the response information if no callback was provided
+ */
+module.exports.patch = async (url, body, options, auth, withCredentials, callback) => {
+  // Create the request object and set the main request parameters
+  const request = new PillowRequest();
+  request.patch(url).setBody(body).options(options).withCredentials(withCredentials);
+
+  // Add the provided type of auth
+  if (auth && auth.username && auth.password) {
+    request.authByUser(auth.username, auth.password);
+  } else if (auth && auth.token) {
+    request.authByToken(auth.token);
+  }
+
+  // Add the callback or return a promise
+  if (callback && typeof callback === 'function') {
+    request.callback(callback).call();
+    return;
+  } else {
+    return await request.call();
+  }
+}
+
+/**
+ * @function delete
+ * @description This function performs a DELETE request using the provided inputs
+ * @param {string} url - the full destination url for the request
+ * @param {{headers?: {[header: string]: string|string[]}, params?: {[param: string]: string|string[]}}} options - the headers and parameters to send with the request
+ * @param {{username?: string, password?: string, token?: string}} [auth] - the username and password or token value to use for authentication
+ * @param {boolean} [withCredentials] - if true and executing in a browser, stores the cookies sent in the request response; if false, does not
+ * @param {(error, response, body) => {}} [callback] - an anonymous function to use as a callback; if provided, executes the callback with the response information
+ * @returns {Promise<{error: Error, response: Object, body: Object}>} Returns a promise of the response information if no callback was provided
+ */
+module.exports.delete = async (url, options, auth, withCredentials, callback) => {
+  // Create the request object and set the main request parameters
+  const request = new PillowRequest();
+  request.delete(url).options(options).withCredentials(withCredentials);
+
+  // Add the provided type of auth
+  if (auth && auth.username && auth.password) {
+    request.authByUser(auth.username, auth.password);
+  } else if (auth && auth.token) {
+    request.authByToken(auth.token);
+  }
+
+  // Add the callback or return a promise
+  if (callback && typeof callback === 'function') {
+    request.callback(callback).call();
+    return;
+  } else {
+    return await request.call();
+  }
+}
